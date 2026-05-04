0b1a95c56825ac3b4d33d0e3df97e336b1388765
cuddihylaura <G00472889@atu.ie>
2026-05-04 17:04:46 +0000

    Enabled HttpClient and set up MovieService with TMDB API

--- src/app/services/movie.spec.ts
+++ src/app/services/movie.spec.ts
@@ -1,16 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-import { Movie } from './movie';
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private apiKey = '64c425a1eac25483f283f2c7bfa217a5';
+  private baseUrl = 'https://api.themoviedb.org/3';
 
-describe('Movie', () => {
-  let service: Movie;
+  constructor(private http: HttpClient) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Movie);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  getTrendingMovies(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`);
+  }
+}
