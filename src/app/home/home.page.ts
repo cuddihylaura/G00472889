5bc3a38613f20f0ebecf1aa8997a4dd3738ce447
cuddihylaura <G00472889@atu.ie>
2026-05-04 19:46:57 +0000

    feat: add searchMovies function to MovieService

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,17 +3,20 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { MovieService } from '../services/movie.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterLink],
+  imports: [IonicModule, CommonModule, RouterLink, FormsModule],
 })
+
 export class HomePage implements OnInit {
   movies: any[] = [];
   studentNumber: string = 'G00472889';
+  searchQuery: string = '';
 
   constructor(private movieService: MovieService) {}
 
@@ -21,6 +24,16 @@ export class HomePage implements OnInit {
     this.movieService.getTrendingMovies().subscribe((data) => {
       this.movies = data.results;
     });
+  } // <--- Make sure there is a closing bracket HERE
+
+  onSearch() { // Now this function is correctly placed inside the Class, but outside ngOnInit
+    if (this.searchQuery.trim() === '') {
+      this.ngOnInit();
+      return;
+    }
+
+    this.movieService.searchMovies(this.searchQuery).subscribe((data) => {
+      this.movies = data.results;
+    });
   }
 }
-
