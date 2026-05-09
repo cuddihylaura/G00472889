47e0f5eb2af9bb467f92a538a43761430bd05109
cuddihylaura <G00472889@atu.ie>
2026-05-09 18:57:09 +0000

    feat: implemented favourites page display and fixed standalone component imports

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+ IonContent, IonHeader, IonTitle, IonToolbar,
+IonList, IonItem, IonThumbnail, IonLabel,
+ IonButton, IonIcon, IonBackButton, IonButtons
+} from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { addIcons } from 'ionicons';
+import { heart, home, person } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-favourites',
-  templateUrl: './favourites.page.html',
-  styleUrls: ['./favourites.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+selector: 'app-favourites',
+templateUrl: './favourites.page.html',
+styleUrls: ['./favourites.page.scss'],
+ standalone: true,
+ imports: [
+ IonContent, IonHeader, IonTitle, IonToolbar,
+IonList, IonItem, IonThumbnail, IonLabel,
+IonButton, IonIcon, IonBackButton, IonButtons,
+CommonModule, FormsModule, RouterModule]
 })
 export class FavouritesPage implements OnInit {
+favouriteMovies: any[] = [];
 
-  constructor() { }
+constructor(private movieService: MovieService) {
 
-  ngOnInit() {
-  }
+// This registers the icons for this page
+addIcons({ heart, home, person });
+ }
 
+ ngOnInit() {
+ this.favouriteMovies = this.movieService.getFavourites();
+}
+
+ ionViewWillEnter() {
+this.favouriteMovies = this.movieService.getFavourites();
+}
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -41,3 +41,4 @@ console.log('Saved to list:', movie.title);
 getFavourites() {
 return this.favourites;
 }
+}
